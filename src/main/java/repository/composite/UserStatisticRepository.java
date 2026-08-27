413e6a9f30dc6a5bf613248c768252813d0cfb93
Leonid Briskindov <lb357@yandex.ru>
2026-08-27 07:48:22 +0300

    Remove unused UserStatisticRepository query

--- src/main/java/repository/composite/UserStatisticRepository.java
+++ src/main/java/repository/composite/UserStatisticRepository.java
@@ -33,30 +33,4 @@ public class UserStatisticRepository {
             return data;
         }
     }
-
-    public static UserStatistic getUserStatistic(int id) throws SQLException {
-        try (Connection connection = Database.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(
-                    "SELECT u.id, u.username, u.status, (SELECT COUNT(*) FROM posts p WHERE p.author_id = u.id) AS post_count, (SELECT COUNT(*) FROM hubs h WHERE h.creator_id = u.id) AS hub_count, (SELECT COUNT(*) FROM votes v WHERE v.voter_id = u.id) AS vote_count, (SELECT COUNT(*) FROM sessions s WHERE s.user_id = u.id) AS active_session_count FROM users u WHERE u.id = ?;"
-            );
-            statement.setInt(1, id);
-
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return new UserStatistic(
-                    new User(
-                        resultSet.getInt(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3)
-                    ),
-                    resultSet.getInt(4),
-                    resultSet.getInt(5),
-                    resultSet.getInt(6),
-                    resultSet.getInt(7)
-                );
-            } else {
-                return null;
-            }
-        }
-    }
 }
